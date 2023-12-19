65852b9d4fc2719d6513391f7502b5d5fd7cd1ee
petter <petter@gmail.com>
2023-12-19 15:48:37 +0800

    init

--- app/api/controller/Pay.php
+++ app/api/controller/Pay.php
@@ -86,7 +86,7 @@ class Pay extends BaseApi
         $pay = new PayModel();
         $info = $pay->getPayType($this->params);
         $temp = empty($info) ? [] : $info;
-        $type = ['ydpay','ydWechatH5','ydWechatScan','ydAlipayH5','ydQuickPay','ydWechatPu','ydWechatMini'];
+        $type = ['ydpay'];
         foreach ($temp[ 'data' ] as $k => $v) {
             array_push($type, $v[ "pay_type" ]);
         }
